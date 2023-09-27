6bb0e083256ba45de293bf80616c71c871045ab8
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2023-09-27 09:46:34 -0400

    Extra Credit for Lab 5 of Unit 5

--- src/NumCompare.java
+++ src/NumCompare.java
@@ -1,5 +1,29 @@
-public class Main {
+import java.util.Scanner;
+
+public class NumCompare {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        int numeroUno = 0;
+        int numeroDos = 0;
+
+        System.out.println("Please enter the first integer to be compared.");
+        numeroUno = scan.nextInt();
+
+        System.out.println("Please enter the second integer to be compared.");
+        numeroDos = scan.nextInt();
+
+        if (numeroUno > numeroDos) {
+            System.out.println(numeroUno + " is greater than " + numeroDos);
+        }
+        else if(numeroDos > numeroUno) {
+            System.out.println(numeroDos + " is greater than " + numeroUno);
+        }
+        else {
+            System.out.println(numeroUno + " is equal to " + numeroDos);
+        }
+
+
+
     }
 }
